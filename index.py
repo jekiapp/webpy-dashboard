3b2a8168b8f296d1481782d90d86db459cb81745
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2015-08-31 13:18:36 +0700

    ganti index dan model

--- index.py
+++ index.py
@@ -2,11 +2,16 @@
 import sys, os, exceptions, traceback,signal
 import web
 
-web.config.debug = False
+web.config.debug = True
 if web.config.debug: os.kill(os.getpid(), signal.SIGINT)
 abspath = os.path.dirname(__file__)
 sys.path.append(abspath)
 os.chdir(abspath)
+
+web.config.db_host = 'localhost';
+web.config.db_user = 'root';
+web.config.db_password = 'jaki';
+web.config.db_database = 'radiaranai';
 
 import controllers.index as con
 urls = (
