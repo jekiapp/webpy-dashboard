3740a317d367667b906f3298454e20ca15d721e1
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2015-08-31 19:36:26 +0700

    a

--- static/js/user.js
+++ static/js/user.js
@@ -2,7 +2,7 @@ function reset_password(){
 	notif("apakah anda yakin",function(){
 		$.post(".",{'reset_password':1},function(response){
 			data = getJSON(response);
-			console.log(data);
+			//console.log(data);
 			notif("ini password barunya : <bold>"+data.password+"</bold>",function(){
 				closeNotif();
 			});
