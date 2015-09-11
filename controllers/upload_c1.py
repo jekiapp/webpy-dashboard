31ad4c08b3cb5c3e003d1416a7b764dd1f22c1b5
Jack <muzakki.ahmad29@gmail.com>
2015-09-11 10:48:00 +0700

    upload

--- controllers/upload_c1.py
+++ controllers/upload_c1.py
@@ -13,12 +13,12 @@ class upload_c1(model):
 		if not domain=="http://www.radiaranai.com" and\
 		 not domain=="http://radiaranai.com":
 			return web.notfound()
-		self.upload(data)
+		return self.upload(data)
 	
 	def upload(self,data):
 		try:
 			nomor = data['nomor']
-			
+
 			filedir = './upload'
 			filename= self.get_filename()
 			buff = StringIO.StringIO()
@@ -27,20 +27,20 @@ class upload_c1(model):
 			im = Image.open(buff)
 			im.save(filedir+"/"+filename,'JPEG')
 			c1_1 = filename
-			
-			
+		
+		
 			filename= self.get_filename()
 			buff = StringIO.StringIO()
-			buff.write(data['c1_1'])
+			buff.write(data['c1_2'])
 			buff.seek(0)
 			im = Image.open(buff)
 			im.save(filedir+"/"+filename,'JPEG')
 			c1_2 = filename
-			
+		
 			sql = "update qc_daftar_nomor set c1_1=%s, c1_2=%s where nomor=%s"
 			self.query(sql,(c1_1,c1_2,nomor))
-		except:
-			return web.notfound()
+			return "<html><body><h3>Upload C1 Model 1 dan 2 berhasil!! </h3><br/>Klik untuk <a href='http://www.radiaranai.com'>[kembali]</a></body></html>"
+		except Exception as e:	return str(e)
 	def get_filename(self,N=15):
 		st = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d_%H_%M_%S')
 		s =  ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
